bd5454bbd7e48d5b41c1f6531de71544fb77a537
TranHoa21 <soc2k3dangyeu@gmail.com>
2024-08-27 22:47:21 +0700

    up

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require('../config/config.json')[process.env.NODE_ENV || 'development'];
 const env = process.env.NODE_ENV || 'development';
 const db = {};
 
